d853584d7797452df5fb6aa5baa340da0c95013f
Asim M Al Twijry <AsimNet@users.noreply.github.com>
2026-01-31 14:36:22 +0300

    feat(topics): add send-to-subscribers action linking to bulk notifications

--- src/Filament/Resources/TopicResource/Pages/EditTopic.php
+++ src/Filament/Resources/TopicResource/Pages/EditTopic.php
@@ -2,7 +2,9 @@
 
 namespace Asimnet\Notify\Filament\Resources\TopicResource\Pages;
 
+use App\Filament\Resources\BulkNotifications\BulkNotificationsResource;
 use Asimnet\Notify\Filament\Resources\TopicResource;
+use Filament\Actions\Action;
 use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
 
@@ -13,6 +15,15 @@ class EditTopic extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
+            Action::make('send_to_subscribers')
+                ->label('إرسال للمشتركين')
+                ->icon('heroicon-o-paper-airplane')
+                ->color('primary')
+                ->url(fn (): string => BulkNotificationsResource::getUrl('create', [
+                    'owner_type' => urlencode($this->record::class),
+                    'owner_id' => $this->record->getKey(),
+                ]))
+                ->openUrlInNewTab(),
             DeleteAction::make(),
         ];
     }
